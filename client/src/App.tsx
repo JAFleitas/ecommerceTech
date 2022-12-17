33f97497ed05db6d72614efd1717c7a5b51ff7c3
Gonzalo Fleitas <psi.gonzalo.fleitas@gmail.com>
2022-12-17 11:09:45 -0300

    adding global styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,30 @@ const GlobalStyle = createGlobalStyle`
   padding: 0;
   margin:0;
   }
+  *::-webkit-scrollbar {
+    -webkit-appearance: none;
+}
+*::-webkit-scrollbar:vertical {
+    width:8px;
+}
+*::-webkit-scrollbar-button:increment {
+    display: none;
+} 
+*::-webkit-scrollbar:horizontal {
+    height: 2px;
+   
+}
+*::-webkit-scrollbar-thumb {
+    background-color: #797979;
+    border-radius: 15px;
+    border: 2px solid #f1f2f3;
+}
+*::-webkit-scrollbar-track {
+    border-radius: 15px;  
+}
+*::-webkit-scrollbar-button {
+    display: none;
+}
 `;
 const Wrapper = styled.div`
   display: flex;
